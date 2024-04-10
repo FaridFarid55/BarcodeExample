ce258bc686720e096f06ad5589b62d19fd55de52
FaridFarid <faridfaridfr5@gmail.com>
2024-04-10 20:46:01 +0200

    Add project files.

---
+++ BarcodeExample2/Item/Item.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeExample2.Item
+{
+    internal class Item
+    {
+        #region  Variabol
+        public int ItemCarogry { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemNaumber { get; set; }
+        public string ItemName { get; set; }
+        #endregion
+
+
+        #region  Method 
+        public string GetFormatCatogryNumber(int Number)
+        {
+            string NewNumber = Number.ToString();
+            string ReturnNumber = string.Empty;
+
+            for (int i = 0; i < 3 - NewNumber.Length; i++)
+            {
+                ReturnNumber += "0";
+            }
+            return ReturnNumber + NewNumber;
+
+        }
+
+        public string GetFormatPrice(decimal price)
+        {
+            string NewNumber = price.ToString();
+            string ReturnNumber = string.Empty;
+
+            for (int i = 0; i < 5 - NewNumber.Length; i++)
+            {
+                ReturnNumber += "0";
+            }
+            return ReturnNumber + NewNumber;
+
+        }
+
+        public string GetFormatNumber(int Number)
+        {
+            string NewNumber = Number.ToString();
+            string ReturnNumber = string.Empty;
+
+            for (int i = 0; i < 3 - NewNumber.Length; i++)
+            {
+                ReturnNumber += "0";
+            }
+            return ReturnNumber + NewNumber;
+
+        } 
+        #endregion
+    }
+}
---
+++ BarcodeExample2/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using BarcodeExample2.Item;
+
+namespace BorcodeExample2
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            // welcome User 
+            Console.WriteLine("Welcome To Boarcode App");
+
+            // item Catogry --> 3
+            // item Price   --> 5
+            // item Number  --> 5
+            // item Name 
+
+            // Barcodes
+            string sBarcodes = @"0010250000020-0110300000200-0210050000320-0120550000520";
+
+            // split - array
+            string[] sBarcodesArray = sBarcodes.Split('-');
+
+            // object new
+            Item Oitem = new Item();
+
+            // Array List
+            List<Item> listsBarcodes = new List<Item>();
+
+            // loop
+            foreach (string item in sBarcodesArray)
+            {
+                Oitem = new Item();
+
+                Oitem.ItemCarogry = Convert.ToInt32(sBarcodes.Substring(0, 3));
+                Oitem.ItemPrice = Convert.ToInt32(sBarcodes.Substring(3, 5));
+                Oitem.ItemNaumber = Convert.ToInt32(sBarcodes.Substring(8, 5));
+
+                // Pleas Enter a UserName 
+                Console.WriteLine("Pleas Enter UserName");
+                Oitem.ItemName = Console.ReadLine();
+
+                listsBarcodes.Add(Oitem);
+            }
+
+            // return
+            string NewBorcode = string.Empty;
+            Item MyItem = new Item();
+
+            // loop
+            foreach (var item in listsBarcodes)
+            {
+                NewBorcode = $"-{MyItem.GetFormatCatogryNumber(Oitem.ItemCarogry)}" +
+                    $"{MyItem.GetFormatPrice(Oitem.ItemPrice)} {MyItem.GetFormatNumber(Oitem.ItemNaumber)}";
+            }
+
+        }
+    }
+}
